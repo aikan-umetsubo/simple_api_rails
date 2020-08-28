38950bd269035d1349618ddee34d61a131a54b2f
aikan-umetsubo <yakinori21@gmail.com>
2020-08-28 18:10:28 +0900

    Todoのモデル、コントローラを追加

---
+++ app/controllers/v1/todos_controller.rb
@@ -0,0 +1,47 @@
+module V1
+  class TodosController < ApplicationController
+    before_action :set_todo, only: [:show, :update, :destroy]
+
+    def index
+      todos = Todo.order(created_at: :desc)
+      render json: { status: 'SUCCESS', message: 'Loaded todos', data: todos }
+    end
+
+    def show
+      render json: { status: 'SUCCESS', message: 'Loaded the todo', data: @todo }
+    end
+
+    def create
+      todo = Todo.new(todo_params)
+      if todo.save
+        render json: { status: 'SUCCESS', data: todo }
+      else
+        render json: { status: 'ERROR', data: todo.errors }
+      end
+    end
+
+    def destroy
+      @todo.destroy
+      render json: { status: 'SUCCESS', message: 'Deleted the todo', data: @todo }
+    end
+
+    def update
+      if @todo.update(todo_params)
+        render json: { status: 'SUCCESS', message: 'Updated the todo', data: @todo }
+      else
+        render json: { status: 'SUCCESS', message: 'Not updated', data: @todo.errors }
+      end
+    end
+
+    private
+
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+
+    def todo_params
+      params.require(:todo).permit(:title, :body)
+    end
+  end
+end
+
